d148ffd5f2089a4c4d8e03da992b45b0e7a7a803
Naloam <2263608676@qq.com>
2025-06-06 14:21:27 +0800

    更新网站和个人资料链接，修改时区设置

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
-  website: "https://che1sy.dev/",
+  website: "https://che1sy.netlify.app/",
   author: "che1sy",
-  profile: "https://che1sy.dev/",
-  desc: "探索技术，分享知识，记录生活",
+  profile: "https://che1sy.netlify.app/",
+  desc: "che1sy的小站",
   title: "che1sy的小站",
   ogImage: "che1sy-og.jpg",
   lightAndDarkMode: true,
@@ -18,5 +18,5 @@ export const SITE = {
   },
   dynamicOgImage: true,
   lang: "zh", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Asia/China", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
